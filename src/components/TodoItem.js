b9df3ec3c8557e5d337e2e3ed8aec867ff2cd077
Park Chan Young <cksdud52017@gmail.com>
2024-05-25 01:24:58 +0900

    todo app v1

---
+++ src/components/TodoItem.js
@@ -0,0 +1,22 @@
+import React from "react";
+import { Col, Row } from "react-bootstrap";
+
+const TodoItem = ({item, updateTask, deleteTask}) => {
+
+  return (
+    <Row>
+      <Col xs={12}>
+        <div className={item.isComplete ? `todo-item-clear` : 'todo-item'}>
+          <div className="todo-content">{item.task}</div>
+
+          <div>
+            <button className="button-delete" onClick={()=>deleteTask(item._id)}>삭제</button>
+            <button className="button-delete" onClick={()=>updateTask(item._id, item.isComplete)}>{item.isComplete ? '끝남' : '안끝남'}</button>
+          </div>
+        </div>
+      </Col>
+    </Row>
+  );
+};
+
+export default TodoItem;
